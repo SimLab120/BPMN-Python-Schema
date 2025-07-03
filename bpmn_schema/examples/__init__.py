858a87ff6e31126dbdea6c4548fcef68b5111147
Seismic Imaging Lab (IITB) <simlab120@gmail.com>
2025-07-03 13:01:52 +0530

    Create __init__.py

---
+++ bpmn_schema/examples/__init__.py
@@ -0,0 +1,4 @@
+from .simple_process import create_simple_approval_process
+from .collaboration import create_collaboration_process
+
+__all__ = ["create_simple_approval_process", "create_collaboration_process"]
